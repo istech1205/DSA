58b65b77d7de3d711f23bf1dc7a65aa6bb4e6957
istech1205 <sam.developer.1205@gmail.com>
2022-04-16 20:51:41 +0530

    selction sort

--- SelectionSort.cpp
+++ SelectionSort.cpp
@@ -4,23 +4,21 @@ using namespace std;
 
 void sort(int a[], int n)
 {
-    for (int i = 0; i < n-2; i++)
-    {
-        int smallest=INT_MAX;
-        for (int j = 0; j <=n-1; j++)
-        {
-            if(a[j]<smallest){
-                smallest=a[j];
-            }
-            
-        }
-        if (a[i] > smallest)
-        {
-            swap(a[i],smallest);
-        }
-        
-        
-    }
+  
+   for (int i = 0; i < n-2; i++)
+   {
+       int smallest=i;
+       for (int j = i+1; j < n-1; j++)
+       {
+           if (a[j]<a[smallest])
+           {
+              smallest=j;
+           }
+           
+       }
+       swap(a[i],a[smallest]);
+   }
+   
     
 }
 
@@ -28,6 +26,7 @@ int main()
 {
 
     int n;
+    cout <<"enter size of array..."<<endl;
     cin >> n;
     int a[n];
     for (int i = 0; i < n; i++)
