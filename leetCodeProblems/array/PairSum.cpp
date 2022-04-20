52e88b5f81f625802f6e3cc2bd7fb2355c1141b0
istech1205 <sam.developer.1205@gmail.com>
2022-04-20 22:48:01 +0530

    20 April

---
+++ leetCodeProblems/array/PairSum.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <math.h>
+#include <vector>
+using namespace std;
+
+vector<vector<int> > main()
+{
+    vector<vector<int> > ans;
+    int n;
+    cout << "Enter size of array.." << endl;
+
+    cin >> n;
+    int arr[n];
+    cout << "Enter items.." << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            if ((arr[i] + arr[j]) == 5)
+            {
+                vector<int> temp;
+                temp.push_back(min(arr[i], arr[j]));
+                temp.push_back(max(arr[i], arr[j]));
+                ans.push_back(temp);
+            }
+        }
+    }
+    cout << "sum is...."+ans;
+
+    return ans;
+}
