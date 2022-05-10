71baffe4f2e284a05109bf110ec918ba12db1351
istech1205 <sam.developer.1205@gmail.com>
2022-05-10 23:45:08 +0530

    10 may

--- leetCodeProblems/array/arrayRotation.cpp
+++ leetCodeProblems/array/arrayRotation.cpp
@@ -4,29 +4,20 @@ using namespace std;
 
 int main()
 {
-    int n;
-    cout << "Enter size of array.." << endl;
-    cin >> n;
-    cout << "Enter items for array.." << endl;
-    int arr[n];
+    int arr[5] = {2, 3, 4, 5, 6};
+    int k = 2;
+    int n = 5;
+    int temp[n];
     for (int i = 0; i < n; i++)
     {
-        cin >> arr[i];
+        int m = (i + k) % n;
+        temp[m] = arr[i];
     }
-    cout << "Enter any element from array..." << endl;
-    int k;
-    cin >> k;
-    if (n == 0 || (k <= 0))
+
+    for (int i = 0; i < n; i++)
     {
-        cout << "errr" << endl;
+        cout << temp[i];
     }
-    int newSize = n - (k + 1);
-    int newArr[n];
-    for (int i = k + 1; i < n; i++)
-    {
-        newArr[i] = arr[i];
-        cout << newArr[i];
-    }
-    
+
     return 0;
 }
